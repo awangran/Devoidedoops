b6b9ecf1a158d6ca29a8332e24b95a5951fbbe86
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-01 17:30:17 -0500

    init comit

---
+++ Devoided/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+        private vec3 pos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
